5b4e28a53adc50db2331be9be8888361fd0cfc8c
kotinash <92477814+kotinash@users.noreply.github.com>
2023-12-24 21:45:44 +0100

    Update ArrayUtils.hpp

--- ArrayUtils.hpp
+++ ArrayUtils.hpp
@@ -18,7 +18,7 @@ namespace Blossom::Utils {
              * @param str The string to search for within the array.
              * @return True if the string is found in the array, false otherwise.
              */
-            static bool contains(char *arr[], char *str) {
+            static bool contains(char* arr[], char* str) {
                 for (int i = 0; arr[i] != nullptr; i++) {
                     std::string element(arr[i]);
 
